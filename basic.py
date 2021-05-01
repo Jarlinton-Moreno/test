f5ce055b096c3dba9c85e00464d3929b62d36673
Jarlinton Moreno Zea <jarlinton.moreno@endava.com>
2021-05-01 16:17:36 -0500

    doing a little change on the python script

--- basic.py
+++ basic.py
@@ -2,7 +2,7 @@ def say_hello():
     print("Testing the conection between, git local instalation with the Endava github account")
     
 
-if __name__ == "main":
+if __name__ == "__main__":
     say_hello()
     
 
